135f9b0d6d3e7ae16497ba7686002c6a67887422
Felipe dos Santos <cxfosfelipe@gmail.com>
2021-08-16 21:35:47 -0500

    Reuse functions and implement While

--- pythonProject3/First Project - GitHub.py
+++ pythonProject3/First Project - GitHub.py
@@ -1,18 +1,22 @@
 # Global variables
-kg_to_pounds = 1 * 2.205
-name_of_unit1 = "pounds"
-
+kg_lbs_factor = 2.205
+lbs_full_name = "pounds"
+kg_full_name = "Kilograms"
 
 # Main function
-def units_of_mass_converter(kg_value):
-    return f"{kg_value} Kg(s) is {kg_value * kg_to_pounds} {name_of_unit1}."
-
+def mass_converter(value, option):
+    str_value = str(value)
+    if option == 1:
+        text = f"\n{str_value} Kg(s) is {value * kg_lbs_factor} {lbs_full_name}."
+    elif option == 2:
+        text = f"\n{str_value} pound(s) is {value / kg_lbs_factor} {kg_full_name}."
+    return text
 
 def validate_and_execute():
     try:
-        user_input_num = float(user_input)
+        user_input_num = float(user_value)
         if user_input_num > 0:
-            calculated_value = units_of_mass_converter(user_input_num)
+            calculated_value = mass_converter(user_input_num, user_option)
             print(calculated_value)
         elif user_input_num == 0:
             print("If you don't know the answer to this one, I don't know what to tell ya")
@@ -21,38 +25,20 @@ def validate_and_execute():
     except ValueError:
         print("Don't be a smart ass, input a number!")
 
-user_input = input("Enter number of Kg(s) and I'll convert it to pounds\n")   #I'm not sure I understand why this is here.
-validate_and_execute()
-
-print("-----------------------------------------------------------------------------------")
-
-# Global variables
-pounds_to_kg = 1 / 2.205
-name_of_unit2 = "Kilograms"
-
-
-# Main function
-def units_of_mass_converter2(pounds_value):
-    return f"{pounds_value} pound(s) is {pounds_value * pounds_to_kg} {name_of_unit2}."
-
-
-def validate_and_execute2():
+while 1==1:
+    print("-----------------------------------------------------------------------------------\n")
+    print("What do you want to convert?\n")
+    print("1 - kg to lbs\n")
+    print("2 - lbs to kg\n")
     try:
-        user_input_num2 = float(user_input2)
-        if user_input_num2 > 0:
-            calculated_value2 = units_of_mass_converter2(user_input_num2)
-            print(calculated_value2)
-        elif user_input_num2 == 0:
-            print("If you don't know the answer to this one, I don't know what to tell ya")
-        elif user_input_num2 < 0:
-            print("Negative mass is only hypothetical, don't get ahead of yourself")
+        user_option = int(input("Enter the option number\n"))
+        if user_option == 1:
+            user_value = input("Enter number of Kg(s) and I'll convert it to pound(s) \n")
+            validate_and_execute()
+        elif user_option == 2:
+            user_value = input("Enter number of Pound(s) and I will convert it to Kg(s) \n")
+            validate_and_execute()
+        else:
+            print("Invalid Option \n")
     except ValueError:
-        print("Don't be a smart ass, input a number!")
-
-
-# Global variable
-user_input2 = input("""Enter number of Pound(s) and I will convert it to Kg(s)\n""")
-validate_and_execute2()
-
-
-print("-----------------------------------------------------------------------------------")
+        print("Invalid Option \n")
